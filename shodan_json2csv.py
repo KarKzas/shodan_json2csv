18feebed901d890c873ac4b29fb78573931b6350
KarKzas <KarKzas@users.noreply.github.com>
2023-10-30 15:20:12 -0500

    Update shodan_json2csv.py

--- shodan_json2csv.py
+++ shodan_json2csv.py
@@ -4,6 +4,10 @@ import re
 import socket
 import struct
 
+# Define file paths
+json_file_path = "shodan_file.json"
+csv_file_path = "shodan_data.csv"
+
 # Define a function to convert an integer IP to a human-readable format
 def int_to_ip(ip_int):
     return socket.inet_ntoa(struct.pack("!I", ip_int))
@@ -58,10 +62,6 @@ def extract_and_write_to_csv(data, csv_writer):
 
     # Write the data to the CSV row
     csv_writer.writerow([ip, port, city, country, org, os, hostnames, cves, isp, product] + list(ntlm_fields.values()))
-
-# Define file paths
-json_file_path = "cleaned.json"
-csv_file_path = "shodan_data.csv"
 
 # Open the JSON file for reading and CSV file for writing
 with open(json_file_path, "r") as json_file, open(csv_file_path, "w", newline="") as csv_file:
